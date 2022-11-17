6fdce64d117e19aeca25c2a364492c5f86781bed
Jair <jairntonio21@hotmail.com>
2022-11-16 21:05:54 -0600

    1.1

---
+++ EvidenciaFinal/ViewControllers/ViewControllerEquipo.swift
@@ -0,0 +1,26 @@
+//
+//  ViewControllerEquipo.swift
+//  EvidenciaFinal
+//
+//  Created by Alumno on 16/11/22.
+//
+
+import UIKit
+import CoreData
+
+class ViewControllerEquipo: UIViewController {
+
+    @IBOutlet weak var tablaEquipo: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nuevoJugador(_ sender: UIBarButtonItem) {
+    }
+    
+   
+    
+
+}
---
+++ EvidenciaFinal/ViewControllers/ViewControllerPartidos.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerPartidos.swift
+//  EvidenciaFinal
+//
+//  Created by Alumno on 16/11/22.
+//
+
+import UIKit
+
+class ViewControllerPartidos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
